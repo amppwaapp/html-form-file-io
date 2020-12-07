d4f596619e408f7139ae63387b6de0aecc654ee6
amppwaapp <75548870+amppwaapp@users.noreply.github.com>
2020-12-07 14:37:09 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -90,7 +90,7 @@ console.log('sw temp1=', temp1);
 			}
 			
 			const keys = await formdata.keys();
-			console.log('sw in 38 keys.length=' + keys.length);			
+			console.log('sw in 38 keys=', keys);			
 			for (let i = 0; i < keys.length; i += 1) {
 				console.log('sw 40 key[' + i + ']=' + keys[i]);								
 			}

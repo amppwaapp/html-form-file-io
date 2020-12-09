105d068bb9e2ee2b4b780e392ecbea18896845c3
amppwaapp <75548870+amppwaapp@users.noreply.github.com>
2020-12-09 09:45:06 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -48,96 +48,92 @@ self.addEventListener('fetch', function(e) {
 	
 		if (e.request.method == 'POST') {
 			console.log('sw ' + call_id + ' ' + line_num++ + ' POST');
+			
+			const parts = url.split('/upload?');
+			if (parts.length !== 2) {
+				const response = new Response(null, response_headers);
+				console.log('sw ' + call_id + ' ' + line_num++ + ' not my url, url=', url);
+				return response;						
+			}
+			const baseurl = parts[0] + '/';			
 
-			e.respondWith(async function() {
-				console.log('sw ' + call_id + ' ' + line_num++ + '');			
+			const formdata = await e.request.formData();
+			console.log('sw ' + call_id + ' ' + line_num++ + ', formdata=', formdata);
 
-				const parts = url.split('/upload?');
-				if (parts.length !== 2) {
-					const response = new Response(null, response_headers);
-					console.log('sw ' + call_id + ' ' + line_num++ + ' not my url, url=', url);
-					return response;						
+			if (! formdata) {
+				const response = new Response(null, response_headers);
+				console.log('sw ' + call_id + ' ' + line_num++ + ' no formdata, response=', response);
+				return response;			
+			}
+
+			const items = await formdata.keys();
+			console.log('sw ' + call_id + ' ' + line_num++ + ' items=', items);
+			let item = items.next();
+			const files = { };
+			while (!item.done) {
+				const key = item.value;
+				console.log('sw ' + call_id + ' ' + line_num++ + ' key=' + key);
+				const candidate = await formdata.get(key)
+				console.log('sw ' + call_id + ' ' + line_num++ + ' candidate=', candidate);		
+				const characterized = candidate.toString();
+				console.log('sw ' + call_id + ' ' + line_num++ + ' characterized=' + characterized);				
+				if (characterized == '[object File]') {
+					console.log('sw ' + call_id + ' ' + line_num++ + ' found a file');					
+					files[candidate.name] = candidate;
 				}
-				const baseurl = parts[0] + '/';			
-
-				const formdata = await e.request.formData();
-				console.log('sw ' + call_id + ' ' + line_num++ + ', formdata=', formdata);
-
-				if (! formdata) {
-					const response = new Response(null, response_headers);
-					console.log('sw ' + call_id + ' ' + line_num++ + ' no formdata, response=', response);
-					return response;			
+				item = items.next();				
+			}
+			const filenames = Object.getOwnPropertyNames(files);
+			console.log('sw ' + call_id + ' ' + line_num++ + ' filenames.length=' + filenames.length);			
+			let successes = 0;
+			let errors = 0;
+			for (let i = 0; i < filenames.length; i += 1) {
+				const filename = filenames[i]; // was sic "console"
+				console.log('sw ' + call_id + ' ' + line_num++ + ' filename=' + filename);
+				if (! filename) {
+					errors += 1;
+					continue;
 				}
+				const file_object = files[filename];
+				console.log('sw ' + call_id + ' ' + line_num++ + ' file_object=', file_object);				
+				const text = await file_object.text();
+				console.log('sw ' + call_id + ' ' + line_num++ + ' text=' + text);
+				// 0-length file is allowed 
 
-				const items = await formdata.keys();
-				console.log('sw ' + call_id + ' ' + line_num++ + ' items=', items);
-				let item = items.next();
-				const files = { };
-				while (!item.done) {
-					const key = item.value;
-					console.log('sw ' + call_id + ' ' + line_num++ + ' key=' + key);
-					const candidate = await formdata.get(key)
-					console.log('sw ' + call_id + ' ' + line_num++ + ' candidate=', candidate);		
-					const characterized = candidate.toString();
-					console.log('sw ' + call_id + ' ' + line_num++ + ' characterized=' + characterized);				
-					if (characterized == '[object File]') {
-						console.log('sw ' + call_id + ' ' + line_num++ + ' found a file');					
-						files[candidate.name] = candidate;
-					}
-					item = items.next();				
-				}
-				const filenames = Object.getOwnPropertyNames(files);
-				console.log('sw ' + call_id + ' ' + line_num++ + ' filenames.length=' + filenames.length);			
-				let successes = 0;
-				let errors = 0;
-				for (let i = 0; i < filenames.length; i += 1) {
-					const filename = filenames[i]; // was sic "console"
-					console.log('sw ' + call_id + ' ' + line_num++ + ' filename=' + filename);
-					if (! filename) {
-						errors += 1;
-						continue;
-					}
-					const file_object = files[filename];
-					console.log('sw ' + call_id + ' ' + line_num++ + ' file_object=', file_object);				
-					const text = await file_object.text();
-					console.log('sw ' + call_id + ' ' + line_num++ + ' text=' + text);
-					// 0-length file is allowed 
+				const init_for_cache_copy = {  };
+				init_for_cache_copy.status = '200';
+				init_for_cache_copy.statusText = 'OK';
+				init_for_cache_copy.headers = new Headers({
+					'Content-Type': 'text/plain', 
+					'Content-Length': text.length
+				});
 
-					const init_for_cache_copy = {  };
-					init_for_cache_copy.status = '200';
-					init_for_cache_copy.statusText = 'OK';
-					init_for_cache_copy.headers = new Headers({
-						'Content-Type': 'text/plain', 
-						'Content-Length': text.length
+				const url = baseurl + filename;
+				console.log('sw ' + call_id + ' ' + line_num++ + ' cache with url=', url);				
+				//const request2cache = new Request(url, {method: 'GET'});
+				const response2cache = new Response(text, init_for_cache_copy);				
+				caches.open(namespace).then(function(cache) {
+					//console.log('sw ' + call_id + ' ' + line_num++ + ' PUT TO PLAY cache put, request2cache=', request2cache);
+					cache.put(url, response2cache).then(function() {
+						console.log('sw ' + call_id + ' ' + line_num++ + ' cache put successful');
 					});
+				} )				
 
-					const url = baseurl + filename;
-					console.log('sw ' + call_id + ' ' + line_num++ + ' cache with url=', url);				
-					//const request2cache = new Request(url, {method: 'GET'});
-					const response2cache = new Response(text, init_for_cache_copy);				
-					caches.open(namespace).then(function(cache) {
-						//console.log('sw ' + call_id + ' ' + line_num++ + ' PUT TO PLAY cache put, request2cache=', request2cache);
-						cache.put(url, response2cache).then(function() {
-							console.log('sw ' + call_id + ' ' + line_num++ + ' cache put successful');
-						});
-					} )				
+				successes += 1;
+			}
 
-					successes += 1;
-				}
-
-				if (successes > 0 && errors == 0) {
-					init.status = '200';
-					init.statusText = 'OK';
-					init.headers = new Headers({
-						'Content-Type': 'application/json'
-					});
-					// , 'Content-Length': text.length
-				}
-				const response2return = new Response({ }, response_headers);								
-				return response2return;					
-
-
+			if (successes > 0 && errors == 0) {
+				init.status = '200';
+				init.statusText = 'OK';
+				init.headers = new Headers({
+					'Content-Type': 'application/json'
+				});
+				// , 'Content-Length': text.length
+			}
+			const response2return = new Response({ }, response_headers);								
+			return response2return;					
 		} // ^ POST
+		
 	} () ); // ^ e.respondWith(async function() {		
 } ); // ^ fetch event listener
 
